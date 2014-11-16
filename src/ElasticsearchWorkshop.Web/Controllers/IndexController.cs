dab6f0e0f8742c39a5ec57cfc7dd3cc047c364d2
Tomas Jansson <tomas.jansson@gmail.com>
2014-11-16 18:50:47 +0100

    extract customers, orders and products from db

--- src/ElasticsearchWorkshop.Web/Controllers/IndexController.cs
+++ src/ElasticsearchWorkshop.Web/Controllers/IndexController.cs
@@ -1,5 +1,11 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
 using System.Web.Http;
+using ElasticsearchWorkshop.Web.Extensions;
+using Nest;
+using WebGrease.Css.Extensions;
 
 namespace ElasticsearchWorkshop.Web.Controllers
 {
@@ -10,7 +16,28 @@ namespace ElasticsearchWorkshop.Web.Controllers
         [HttpPost]
         public HttpResponseMessage Post()
         {
-            return Request.CreateResponse("Indexing");
+            using (var context = new NORTHWNDEntities())
+            {
+                var customers = context.Customers.ToDocuments().ToList();
+                var products = context.Products.ToDocuments().ToList();
+                var orders = context.Orders.ToDocuments().ToList();
+                var indexModel = new IndexModel(customers, products, orders);
+                return Request.CreateResponse(indexModel);
+            }
+        }
+    }
+
+    public class IndexModel
+    {
+        public IEnumerable<Customer> Customers { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+
+        public IndexModel(IEnumerable<Customer> customers, IEnumerable<Product> products, IEnumerable<Order> orders)
+        {
+            Customers = customers;
+            Products = products;
+            Orders = orders;
         }
     }
 }
---
+++ src/ElasticsearchWorkshop.Web/Extensions/ModelExtensions.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElasticsearchWorkshop.Web.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static IEnumerable<Customer> ToDocuments(this IEnumerable<Customers> dbCustomers)
+        {
+            return dbCustomers.Select(y => y.ToDocument());
+        }
+
+        public static Customer ToDocument(this Customers dbCustomer)
+        {
+            return new Customer
+            {
+                Id = dbCustomer.CustomerID,
+                Country = dbCustomer.Country,
+                CompanyName = dbCustomer.CompanyName,
+                City = dbCustomer.City,
+                Address = dbCustomer.Address,
+                Region = dbCustomer.Region,
+                ContactName = dbCustomer.ContactName
+            };
+        }
+
+        public static IEnumerable<Product> ToDocuments(this IEnumerable<Products> dbProducts)
+        {
+            return dbProducts.Select(y => y.ToDocument());
+        }
+
+        public static Product ToDocument(this Products dbProduct)
+        {
+
+            return new Product()
+            {
+                Id = dbProduct.ProductID,
+                Category = dbProduct.Categories.ToDocument(),
+                Name = dbProduct.ProductName,
+                Price = dbProduct.UnitPrice
+            };
+        }
+
+        public static Category ToDocument(this Categories dbCategory)
+        {
+            return new Category()
+            {
+                Name = dbCategory.CategoryName,
+                Description = dbCategory.Description,
+                Id = dbCategory.CategoryID
+            };
+        }
+
+        public static IEnumerable<Order> ToDocuments(this IEnumerable<Orders> dbOrders)
+        {
+            return dbOrders.Select(y => y.ToDocument());
+        }
+
+        public static Order ToDocument(this Orders dbOrder)
+        {
+            return new Order()
+            {
+                CustomerId = dbOrder.CustomerID,
+                Id = dbOrder.OrderID,
+                OrderDate = dbOrder.OrderDate,
+                OrderLines = dbOrder.Order_Details.ToDocuments()
+            };
+        }
+
+        public static IEnumerable<OrderLine> ToDocuments(this IEnumerable<Order_Details> dbOrderLines)
+        {
+            return dbOrderLines.Select(y => y.ToDocument());
+        }
+
+        public static OrderLine ToDocument(this Order_Details dbOrderLine)
+        {
+            return new OrderLine()
+            {
+                Quantity = dbOrderLine.Quantity,
+                UnitPrice = dbOrderLine.UnitPrice,
+                ProductName = dbOrderLine.Products.ProductName,
+                ProductId = dbOrderLine.Products.ProductID
+            };
+        }
+    }
+
+    public class OrderLine
+    {
+        public short Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string ProductName { get; set; }
+        public int ProductId { get; set; }
+    }
+
+    public class Order
+    {
+        public string CustomerId { get; set; }
+        public int Id { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public IEnumerable<OrderLine> OrderLines { get; set; }
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public decimal? Price { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class Customer
+    {
+        public string Country { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Region { get; set; }
+        public string ContactName { get; set; }
+        public string Id { get; set; }
+    }
+}
