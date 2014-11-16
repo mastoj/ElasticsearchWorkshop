efd7501b55c85c0e681e67d794b6be0dc176cc3a
Tomas Jansson <tomas.jansson@gmail.com>
2014-11-16 17:18:32 +0100

    initial commit

---
+++ src/ElasticsearchWorkshop.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ElasticsearchWorkshop.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/ElasticsearchWorkshop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElasticsearchWorkshop.Web.WebApiApplication" Language="C#" %>
