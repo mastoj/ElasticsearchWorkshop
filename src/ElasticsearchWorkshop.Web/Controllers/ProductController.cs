08898d0579365d58652ec71cb1efbab0a62d7a7f
Tomas Jansson <tomas.jansson@gmail.com>
2014-11-18 20:03:46 +0100

    facet added

--- src/ElasticsearchWorkshop.Web/Controllers/ProductController.cs
+++ src/ElasticsearchWorkshop.Web/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Web.Http;
 using ElasticsearchWorkshop.Web.Models;
+using Nest;
 
 namespace ElasticsearchWorkshop.Web.Controllers
 {
@@ -12,8 +14,14 @@ namespace ElasticsearchWorkshop.Web.Controllers
         public HttpResponseMessage Get(string query)
         {
             var result = _indexer.Search<Product>(ss => ss
-                .QueryString(query));
-            return Request.CreateResponse(result.Documents);
+                .QueryString(query)
+                .Aggregations(aggs => aggs
+                    .Terms("categories", s => s
+                        .Field(f => f.Category.Name)
+                        .Aggregations(s2 => s2 
+                            .Terms("categoryid", s3 => s3.Field(p2=> p2.Category.Id))))));
+            var productQueryViewModel = new ProductQueryViewModel(result.Documents, result.Aggregations);
+            return Request.CreateResponse(productQueryViewModel);
         }
 
         [Route]
@@ -28,6 +36,18 @@ namespace ElasticsearchWorkshop.Web.Controllers
         public HttpResponseMessage Delete(string id)
         {
             return Request.CreateResponse("Deleting product with id: " + id);
+        }
+    }
+
+    public class ProductQueryViewModel
+    {
+        public IEnumerable<Product> Documents { get; set; }
+        public IDictionary<string, IAggregation> Aggregations { get; set; }
+
+        public ProductQueryViewModel(IEnumerable<Product> documents, IDictionary<string, IAggregation> aggregations)
+        {
+            Documents = documents;
+            Aggregations = aggregations;
         }
     }
 }
