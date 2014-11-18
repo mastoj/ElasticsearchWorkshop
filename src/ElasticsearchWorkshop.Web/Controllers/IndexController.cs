fea1c99889594de6f14ff9a91850376341c2ebd7
Tomas Jansson <tomas.jansson@gmail.com>
2014-11-18 20:36:38 +0100

    simple aliasing done

--- src/ElasticsearchWorkshop.Web/Controllers/IndexController.cs
+++ src/ElasticsearchWorkshop.Web/Controllers/IndexController.cs
@@ -4,12 +4,22 @@ using System.Net.Http;
 using System.Web.Http;
 using ElasticsearchWorkshop.Web.Extensions;
 using ElasticsearchWorkshop.Web.Models;
+using Nest;
+using WebGrease.Css.Extensions;
 
 namespace ElasticsearchWorkshop.Web.Controllers
 {
     [RoutePrefix("api/index")]
     public class IndexController : BaseController
     {
+        public static string _indexBaseName = "elasticworkshop";
+        public static int _indexVersion = 0;
+
+        public static string GetIndexName(string name, int version)
+        {
+            return string.Format("{0}_{1}", name, version);
+        }
+
         [Route]
         [HttpPost]
         public HttpResponseMessage Post()
@@ -21,11 +31,24 @@ namespace ElasticsearchWorkshop.Web.Controllers
                 var orders = context.Orders.ToDocuments().ToList();
                 var indexModel = new IndexModel(customers, products, orders);
 
-                _indexer.DeleteIndex(s => s.Index("elasticworkshop"));
-                customers.ForEach(c => _indexer.Index(c));
-                products.ForEach(p => _indexer.Index(p));
-                orders.ForEach(o => _indexer.Index(o));
+                var newIndexVersion = _indexVersion + 1;
+                var newIndexName = GetIndexName(_indexBaseName, newIndexVersion);
+                customers.ForEach(c => _indexer.Index(c, index => index.Index(newIndexName)));
+                products.ForEach(p => _indexer.Index(p, index => index.Index(newIndexName)));
+                orders.ForEach(o => _indexer.Index(o, index => index.Index(newIndexName)));
 
+                var oldIndexes = _indexer.GetIndicesPointingToAlias(_indexBaseName);
+                var result =_indexer.Alias(y =>
+                {
+                    var x = y
+                        .Add(add => add.Index(newIndexName).Alias(_indexBaseName));
+                    if (oldIndexes.Any())
+                    {
+                        oldIndexes.ForEach(oi => x = x.Remove(rem => rem.Index(oi).Alias(_indexBaseName)));
+                    }
+                    return x;
+                });
+                _indexVersion = newIndexVersion;
                 return Request.CreateResponse(indexModel);
             }
         }
